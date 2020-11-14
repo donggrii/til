a333202d7714b7245a9139e989b0636229d9c2d9
Daniel <54230911+DongChan-Lee@users.noreply.github.com>
2020-11-14 21:29:22 +0900

    Add files via upload

---
+++ R을 활용한 데이터분석 교육/01Lecture.R
@@ -0,0 +1,225 @@
+# Ctrl+Shift+c : 주석 적용
+# 1. 연산자(Operator) ----
+# 1.1 산술 연산자 ----
+# +, -, *, /, **, ^, %%, %/%
+# 새로운 변수(열, Feature) 만들 때에 사용함
+3 + 4
+3 - 4
+3 * 4
+3 / 4     
+3 ** 4    
+3 ^ 4     
+13 %% 4   # 나머지
+13 %/% 4  # 몫
+
+
+# 1.2 할당 연산자 ----
+# <-, ->, =
+# 저장하는 기능
+# <-, -> : 일반적인 저장 기능
+# =      : "함수 안에서" argument를 저장하는 기능
+# alt + - : '<-' 생성
+x <- rnorm(n = 100, mean = 10, sd = 2)
+x
+
+# 참고
+# rnorm()     : 함수
+# n, mean, sd : rnorm() 함수의 argument
+10 -> y
+y
+
+
+# 1.3 비교 연산자 ----
+# >, >=, <, <=, ==, !=, !
+# 전체 데이터에서 조건에 맞는 데이터를 추출할 때 사용
+3 > 4
+3 >= 4
+3 < 4
+3 <= 4
+3 == 4
+3 != 4
+!(3 == 4)
+
+
+# 1.4 논리 연산자 ----
+# &, |(Pipe)
+# 전체 데이터 중에서 조건에 맞는 데이터를 추출할 때 사용
+# 조건 : 2개 이상일 때
+# & : and
+# | : or
+# Alt + Shift + ↓ : 복사
+# Ctrl + D : 삭제
+(3 > 4) & (5 > 4)
+(3 > 4) | (5 > 4)
+
+
+# 2. 데이터의 유형(Type of Data) ----
+# 자료
+# a. 질적 자료(범주형 자료) : 문자, 숫자(but 숫자의 의미 X) : categorical
+# b. 양적 자료(수치형 자료) : 숫자(숫자의 의미 O)
+
+# 2.1 수치형(Numeric) ----
+# (1) 정수(integer)
+# (2) 실수(double)
+x1 <- 10
+x2 <- 10.2
+
+
+# 2.2 문자형(Character) ----
+x3 <- 'I see you.'
+x4 <- "Love is not feeling."
+
+
+# 2.3 논리형(Logical) ----
+x5 <- TRUE     # T
+x6 <- FALSE    # F
+
+# NA(Not Available) = Missing Value = 결측치, 결측값
+# 데이터 유형 확인하는 함수(논리형 결과값 반환)
+# : mode(), is.numeric(), is.character(), is.logical(), is.null(), is.na() 등
+mode(x1)
+mode(x3)
+mode(x5)
+# 데이터 유형 변경하는 함수
+# : as.numeric(), as.character(), as.logical() 등
+
+# 3. Data ----
+# 3.1 Vector ----
+# 수치형 자료
+# 하나의 열로 구성되어 있음
+# 1차원 구조
+# 하나의 데이터 유형만 가짐
+# 집단으로 인식하지 않음
+# 데이터 분석의 가장 기본 단위
+
+# (1) Vector의 생성 ----
+# c()
+v1 <- c(3, 10, 12)
+v2 <- c("Kim", "Lee","Park")
+v3 <- c(v1, v2)
+
+v4 <- -3.3:5
+
+# seq()
+v1 <- seq(from = 1, to = 5, by = 0.5)
+
+# (2) Vector의 속성 ----
+# 데이터의 개수 : length(Vector)
+length(v1)
+
+# 데이터의 추출 : 슬라이싱(slicing)
+weight <- c(57, 81, 65, 49, 72)
+weight[1]
+weight[2:4]
+weight[-c(1,4,5)]
+
+
+# 3.2 Factor ----
+# 범주형 자료
+# Vector와 "집단으로 인식함"이라는 부분에서만 다름
+
+
+# 3.3 Data.Frame ----
+# 행과 열로 구성되어 있음
+# 2차원 구조
+# 다른 열은 다른 데이터 유형을 가질 수 있음
+
+# 3.4 List ----
+# 중급 이상의 데이터 분석의 결과는 대부분 List
+# 1차원 구조
+# 가장 유연한 형태의 데이터
+
+
+# 4. 외부 데이터 읽어오기 ----
+# 4.1 txt ----
+# data <- read.table(file             = "directory/filename.txt", 
+#                    header           = TRUE or FALSE,
+#                    sep              = " " or "," or "\t",
+#                    stringsAsFactors = TRUE or FALSE)
+# header = TRUE이면 data에 있는 column명을 그대로 쓴다는 것
+# stringsAsFacotrs = TRUE이면 문자로 되어 있는 열을 Factor로 만든다는 것
+favor <- read.table(file             = "C:/KCISA/favor.txt",
+                    header           = TRUE,
+                    sep              = ",",
+                    stringsAsFactors = TRUE)
+favor
+
+# favor라는 data frame에서 하나의 열만 보고 싶을 때 $ 사용
+mean(favor$culture)
+
+# 4.2 csv ----
+# data <- read.csv(file             = "directory/filename.csv",
+#                  header           = TRUE or FALSE,
+#                  stringsAsFactors = TRUE or FALSE)
+hope <- read.csv(file             = "C:/KCISA/hope.csv",
+                 header           = TRUE,
+                 stringsAsFactors = TRUE)
+hope
+
+# 4.3 xlsx ----
+# R의 기본 기능에서 못 읽어옴
+# R의 새로운 기능을 설치하고 로딩하기를 해야 함
+# 패키지 설치하기 : install.packages("패키지명")
+install.packages("readxl")
+
+# 패키지 로딩하기 : library("패키지명")
+# install해도 library 사용할 때마다 불러와야 함
+library(readxl)
+
+# data <- readxl::read_excel(path      = "directory/filename.xlsx",
+#                            sheet     = "sheet_name" or sheet_index,
+#                            col_names = TRUE or FALSE)
+culture <- readxl::read_excel(path      = "C:/KCISA/culture.xlsx",
+                              sheet     = "DM",
+                              col_names = TRUE)
+View(culture)
+
+# Alt + ↓, ↑ : 코드 내 이동
+culture2 <- readxl::read_excel(path      = "C:/KCISA/culture.xlsx",
+                               sheet     = 1,
+                               col_names = TRUE)
+View(culture2)
+
+# 작업공간(Working Directory) ----
+# (1) 현재 설정된 작업공간 : getwd()
+getwd()
+
+# (2) 새로운 작업공간 설정하기 : setwd("directory")
+setwd("C:/KCISA/")
+
+culture3 <- readxl::read_excel(path      = "culture.xlsx",
+                               sheet     = 1,
+                               col_names = TRUE)
+View(culture3)
+
+
+# 5. 외부 데이터로 저장하기 ----
+# 5.1 txt ----
+# 5.2 csv ----
+# 5.3 xlsx ----
+# 패키지 : writexl
+install.packages("writexl")
+library(writexl)
+
+# writexl::write_xlsx(data, 
+#                     path = "directory/filename.xlsx")
+writexl::write_xlsx(culture,
+                    path = "culture_2020_1112_1037.xlsx")
+
+
+# 6. RData로 저장하고 불러오기 ----
+# 6.1 RData로 저장하기 ----
+# RAM에 있는 rdata를 Hard Drive(HDD)에 rdata로 저장하기
+# save(data, file = "directory/filename.RData")
+save(culture, file = "culture_2020_1112_1050.RData")
+
+
+# 6.2 Rdata로 불러오기 ----
+# HDD에 있는 rdata를 RAM에 rdata로 올리는 기능
+# load(file = "directory/filename.RData")
+load(file = "culture_2020_1112_1050.RData")
+
+
+
+
+
