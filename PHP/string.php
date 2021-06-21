e81d4cd7d6bb7dd05c69d1a48e7e2c3477d428b5
Daniel <54230911+DongChan-Lee@users.noreply.github.com>
2021-06-21 23:52:19 +0900

    Add files via upload

---
+++ PHP/string.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>String & String Operator</h1>
+        <?php
+            echo "Hello \"W\"orld 동찬";
+        ?>
+
+        <h2>concatenation operator</h2>
+        <?php
+            echo "Hello " . "world";
+        ?>
+
+        <h2>String length function</h2>
+        <?php
+            echo strlen("동찬");
+            echo strlen("Hello");
+        ?>
+    </body>
+</html>
