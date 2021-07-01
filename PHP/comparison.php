2065ad828b83ccce7fb3fa3819587a7f8391a0be
Daniel <54230911+DongChan-Lee@users.noreply.github.com>
2021-07-01 14:22:54 +0900

    Add files via upload

---
+++ PHP/comparison.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Comparison Operators & Boolean data type</h1>
+        <?php
+            var_dump(1+1);      // int(2)
+            var_dump(1==1);     // bool(true)
+        ?>
+    </body>
+</html>
